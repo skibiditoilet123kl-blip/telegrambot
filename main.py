c18313c079c6d1ad1dfb68778047e90d59cb9f91
skibiditoilet123kl-blip <skibiditoilet123kl@gmail.com>
2026-03-06 22:02:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import LabeledPrice
 
 TOKEN = "8450764497:AAESpLs3rP-BDtQnIIkqg6nhBdtPsDfvG-w"
 
@@ -9,7 +10,25 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("Бот работает ✅")
+    await message.answer("Привет! Напиши /buy чтобы купить ⭐")
+
+@dp.message(Command("buy"))
+async def buy(message: types.Message):
+    prices = [LabeledPrice(label="Товар", amount=50)]
+    await bot.send_invoice(
+        chat_id=message.chat.id,
+        title="Покупка",
+        description="Цифровой товар",
+        payload="product1",
+        provider_token="",
+        currency="XTR",
+        prices=prices
+    )
+
+@dp.message(lambda msg: msg.successful_payment)
+async def payment(msg: types.Message):
+    await msg.answer("Оплата получена ⭐")
+    await msg.answer("Вот ваш товар: https://site.com/file.zip")
 
 async def main():
     await dp.start_polling(bot)

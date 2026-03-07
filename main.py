33635d18c0e3c67145b91c506948b2d451ac2a9a
skibiditoilet123kl-blip <skibiditoilet123kl@gmail.com>
2026-03-07 10:05:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,28 @@ from aiogram.types import (
 )
 
 TOKEN = "8614684488:AAFlWlgEm6CcuVaq5kJe8te0PuYHV0Wead8"
+ADMIN_ID = 5349252067  # вставь свой telegram id
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
 # список подарков
 gifts = {
-    "bear": ("🧸 Мишка", 9),
-    "giftbox": ("🎁 Подарочная коробка", 15),
-    "ring": ("💍 Обручальное кольцо", 30),
-    "diamond": ("💎 Бриллиант", 30),
-    "heart": ("❤️ Сердце", 9),
-    "flowers": ("💐 Букет цветов", 20),
-    "rose": ("🌹 Роза", 10),
-    "champagne": ("🍾 Шампанское", 25),
-    "cup": ("🏆 Кубок", 30),
-    "rocket": ("🚀 Ракета", 25),
+    "bear": ("🧸 Мишка", 15),
+    "giftbox": ("🎁 Подарочная коробка", 25),
+    "ring": ("💍 Обручальное кольцо", 100),
+    "diamond": ("💎 Бриллиант", 100),
+    "heart": ("❤️ Сердце", 15),
+    "flowers": ("💐 Букет цветов", 50),
+    "rose": ("🌹 Роза", 25),
+    "champagne": ("🍾 Шампанское", 50),
+    "cup": ("🏆 Кубок", 100),
+    "rocket": ("🚀 Ракета", 50),
 }
 
-# клавиатура
+sales = []
+
+# клавиатура подарков
 def gifts_keyboard():
     buttons = []
     for key, value in gifts.items():
@@ -41,10 +44,11 @@ def gifts_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
 
+# старт
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(
-        "✅ Отлично! \n\nвыбери подарок:",
+        "✅ Отлично! Теперь вы можете использовать бота.\n\nВыбери подарок:",
         reply_markup=gifts_keyboard()
     )
 
@@ -63,7 +67,7 @@ async def gift_selected(callback: types.CallbackQuery):
         title=name,
         description=f"{name} за {price} звёзд",
         payload=gift_id,
-        provider_token="",  # для Stars оставляем пустым
+        provider_token="",
         currency="XTR",
         prices=prices
     )
@@ -86,6 +90,46 @@ async def successful_payment(message: types.Message):
         await message.answer(
             "❌ Ошибка оплаты.\nПопробуйте ещё раз."
         )
+        await bot.send_message(
+            ADMIN_ID,
+            f"💰 Новая покупка\n\n"
+            f"Пользователь: {message.from_user.id}\n"
+            f"Подарок: {name}\n"
+            f"Цена: {price}⭐"
+        )
+
+
+# админ панель
+@dp.message(Command("admin"))
+async def admin(message: types.Message):
+
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    await message.answer(
+        "👑 Админ панель\n\n"
+        "/sales — посмотреть продажи"
+    )
+
+
+# список продаж
+@dp.message(Command("sales"))
+async def sales_list(message: types.Message):
+
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    if not sales:
+        await message.answer("Продаж пока нет")
+        return
+
+    text = "📊 Продажи:\n\n"
+
+    for s in sales:
+        text += f"{s[0]} купил {s[1]} за {s[2]}⭐\n"
+
+    await message.answer(text)
+
 
 async def main():
     await dp.start_polling(bot)
